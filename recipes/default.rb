24399a8f95348752b48323a46e214f011e6668e0
JamesJJ <James_Hooper@htc.com>
2015-03-09 14:45:32 +0800

    skip if done already

--- recipes/default.rb
+++ recipes/default.rb
@@ -15,6 +15,9 @@ end
 
 node['source_install']['install'].each do |_item|
 
+  _success_file = File.join(node['source_install'][_item]['working_dir'],"#{_item.gsub(/[^a-zA-Z0-9\_\-]+/,'')}-CHEF_COMPLETED_SOURCE_INSTALL")
+  next if File.exists?(_success_file)
+
   File.umask(node['source_install'][_item]['umask'] || 0o022)
 
   node['source_install'][_item]['directories'].each do |_dir|
@@ -54,6 +57,13 @@ node['source_install']['install'].each do |_item|
     environment "PATH" => "/bin:/usr/bin:/usr/sbin:/sbin"
     command node['source_install'][_item]['install_command']
     cwd node['source_install'][_item]['working_dir']
+  end
+
+  file _success_file do
+    owner node['source_install'][_item]['owner'] || 'root'
+    group node['source_install'][_item]['group'] || 'root'
+    mode '0400'
+    content Date.today().to_s
   end
 
 end
